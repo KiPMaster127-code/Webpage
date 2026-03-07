4eed2c792a04a9864c4109280606ac31fd5c54b5
Misha <misha.azaranka@gmail.com>
2026-03-06 20:07:01 -0500

    fixed transparent background for menu button

--- menuscript.js
+++ menuscript.js
@@ -1,15 +1,15 @@
 const menuButton = document.getElementById("navToggle");
-const sideMenu = document.getElementById("sideMenu");
-const overlay = document.getElementById("overlay");
+const sideMenu   = document.getElementById("sideMenu");
+const overlay    = document.getElementById("overlay");
 
 menuButton.addEventListener("click", () => {
   sideMenu.classList.add("open");
-  overlay.style.display = "block";
+  overlay.classList.add("show");   
   menuButton.style.display = "none";
 });
 
 overlay.addEventListener("click", () => {
   sideMenu.classList.remove("open");
-  overlay.style.display = "none";
+  overlay.classList.remove("show"); 
   menuButton.style.display = "block";
 });
